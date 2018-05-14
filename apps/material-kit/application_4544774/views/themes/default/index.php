af71459d66e65213c01b51b7b81ee6af5ab11de5
Adi <developer@rosoftware.ro>
2018-05-14 22:38:49 +0300

    Material-Kit App - added download button on index page

--- apps/material-kit/application_4544774/views/themes/default/index.php
+++ apps/material-kit/application_4544774/views/themes/default/index.php
@@ -6,9 +6,8 @@
                     <h1 class="title">Your Story Starts With Us.</h1>
                     <h4>Every landing page needs a small description after the big bold title, that&apos;s why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
                     <br>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg">
-                        <i class="fa fa-play"></i> Watch video
-                    </a>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-round">Watch video</a>
+                    <a href="https://github.com/rosoftdeveloper/appseed/tree/master/apps/material-kit" class="btn btn-danger btn-round"></i>Download</a>
                 </div>
             </div>
         </div>
