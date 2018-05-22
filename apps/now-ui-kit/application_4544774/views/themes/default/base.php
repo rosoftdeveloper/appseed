d34d52a4eff0d6b83bd63ce81bdc2120faa1a6f4
Adi <developer@rosoftware.ro>
2018-05-22 21:30:32 +0300

    Added analytics for 3 demos: Now-UI, Material-Kir and Paper-Kit

--- apps/now-ui-kit/application_4544774/views/themes/default/base.php
+++ apps/now-ui-kit/application_4544774/views/themes/default/base.php
@@ -19,6 +19,18 @@
     </script>    
     <!-- Facebook plugin END -->
 
+    <!-- Google  Analitics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-104843706-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <!-- Google  Analitics END -->
+
 </head>
 
 <body class="<?php 
