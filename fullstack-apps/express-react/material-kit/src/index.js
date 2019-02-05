29fee7cd7a2668ea9156a3402428f52d51f05314
Adi <developer@rosoftware.ro>
2019-02-05 20:19:11 +0200

    Update Express [ React / Vue ] starters

---
+++ fullstack-apps/express-react/material-kit/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import indexRoutes from "routes/index.jsx";
+
+import "assets/scss/material-kit-react.css?v=1.3.0";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
