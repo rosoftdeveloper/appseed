6b0e507d5143247627806c31ca31a8d23431b5b6
Adi <developer@rosoftware.ro>
2019-02-13 09:40:34 +0200

    Flask Starter - works wit Vue frontend

--- starter-flask/app.py
+++ starter-flask/app.py
@@ -1,42 +1,87 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_jwt_simple import (
     JWTManager, jwt_required, create_jwt, get_jwt_identity
 )
+'''
+const user1 = {
+    id        : 1,
+    name      : 'John',
+    surname   : 'Doe',
+    email     : 'demo@appseed.us',
+    password  : 'demo'
+};
+
+const user2 = {
+    id        : 2,
+    name      : 'George',
+    surname   : 'Clooney',
+    email     : 'demo2@appseed.us',
+    password  : 'demo'
+};
+'''
+
+user1 = { 'id': 1, 'name' : 'John'  , 'surname' : 'Doe'     , 'email' : 'demo@appseed.us'   , 'password' : 'demo'  }
+user2 = { 'id': 2, 'name' : 'George', 'surname' : 'Clooney' , 'email' : 'demo2@appseed.us'  , 'password' : 'demo'  }
+
+Users = {
+    'demo@appseed.us'  : user1,
+    'demo2@appseed.us' : user2,
+}
 
 app = Flask(__name__)
+CORS(app)
 
 # Setup the Flask-JWT-Simple extension
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 jwt = JWTManager(app)
 
-
 # Provide a method to create access tokens. The create_jwt()
 # function is used to actually generate the token
-@app.route('/login', methods=['POST'])
+@app.route('/api/users/login', methods=['POST', 'OPTIONS'])
 def login():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
 
-    params = request.get_json()
+    #username = 'demo@appseed.us'
+    #password = 'demo'
 
-    username = params['user']['email']
-    password = params['user']['password']
+    #user = {'_id': 1, "email": 'demo@appseed.us', 'name' : "John", 'surname' : "Doe", "token" : create_jwt(identity=username) } 
+    #ret  = {'user': user }
 
-    if not username:
-        return jsonify({"msg": "Missing username parameter"}), 400
-    if not password:
-        return jsonify({"msg": "Missing password parameter"}), 400
+    #return jsonify(ret), 200
 
-    if username != 'demo@appseed.com': 
-        return jsonify({"msg": "Bad username"}), 401
+    ###########################################################
+    #if not request.is_json:
+    #    return jsonify({'errors': {'general' : 'format error (expected JSON)' }}), 400
 
-    if password != 'demo': 
-        return jsonify({"msg": "Bad pass"}), 401
+    username = None
+    password = None
 
-    # Identity can be any data that is json serializable
-    ret = {'jwt': create_jwt(identity=username)}
+    try:
+        params = request.get_json()
+
+        username = params['user']['email']
+        password = params['user']['password']
+    
+    # catch JSON format and missing keys (email / password)
+    except:
+        return jsonify({'errors': {'general' : 'Format error ' }}), 400
+
+    if not username in Users:
+        return jsonify({'errors': {'email' : 'User or email doesn\'t exist' }}), 400
+
+    user = Users[ username ] # aka email
+
+    if not password or password != user['password'] :
+        return jsonify({'errors': {'password' : 'Password is invalid' }}), 400 
+
+    # inject token
+    user["token"] = create_jwt(identity=username)
+    
+    # build response
+    ret  = { 'user': user }
+
+    # All good, return response
     return jsonify(ret), 200
-
 
 # Protect a view with jwt_required, which requires a valid jwt
 # to be present in the headers.
@@ -47,4 +92,4 @@ def protected():
     return jsonify({'hello_from': get_jwt_identity()}), 200
 
 if __name__ == '__main__':
-    app.run()
+	app.run(host='0.0.0.0', debug=True)
